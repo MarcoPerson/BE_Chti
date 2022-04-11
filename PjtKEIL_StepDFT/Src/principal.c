bd08bc821c7b406f60842316d5a9bd4e9a518b6c
Komlan Epe-Nsin Merveilles Agbeti-Messan <agbeti-m@insa-toulouse.fr>
2022-04-11 12:30:27 +0200

    DFT en cours de Realisation: A finir prochainement

---
+++ PjtKEIL_StepDFT/Src/principal.c
@@ -0,0 +1,31 @@
+
+
+#include "DriverJeuLaser.h"
+
+extern int DFT_ModuleAuCarre(short int * Signal64ech, char k);
+extern short LeSignal;
+int TabValeur[64];
+
+int main(void)
+{
+
+// ===========================================================================
+// ============= INIT PERIPH (faites qu'une seule fois)  =====================
+// ===========================================================================
+
+// Après exécution : le coeur CPU est clocké à 72MHz ainsi que tous les timers
+CLOCK_Configure();
+	
+for(int i = 0; i < 64; i++){
+	TabValeur[i] = DFT_ModuleAuCarre(&LeSignal, i);
+}
+
+
+//============================================================================	
+	
+	
+while	(1)
+	{
+	}
+}
+
